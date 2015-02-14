b79eae1b6168fd35af3422ccb2ea227c43879a05
Soffolk Zhu <zlx.star@gmail.com>
2015-02-14 15:44:32 +0800

    update url because of weixin 👻

--- lib/omniauth/strategies/weixin.rb
+++ lib/omniauth/strategies/weixin.rb
@@ -5,7 +5,7 @@ module OmniAuth
     class Weixin < OmniAuth::Strategies::OAuth2
       option :client_options, {
         :site => 'https://api.weixin.qq.com',
-        :authorize_url => 'https://open.weixin.qq.com/connect/oauth2/authorize',
+        :authorize_url => 'https://open.weixin.qq.com/connect/qrconnect',
         :token_url => "https://api.weixin.qq.com/sns/oauth2/access_token"
       }
 
@@ -16,12 +16,11 @@ module OmniAuth
       end
 
       def authorize_params
-        options[:scope] = "snsapi_userinfo" if options[:scope].nil?
-
+        options[:scope] = "snsapi_login" if options[:scope].nil?
         {
-          :appid => options.client_id, 
-          :redirect_uri => callback_url, 
-          :response_type => 'code', 
+          :appid => options.client_id,
+          :redirect_uri => callback_url,
+          :response_type => 'code',
           :scope => options[:scope]
         }
       end
@@ -38,21 +37,21 @@ module OmniAuth
           {:mode => :query, :param_name => 'access_token'}
         )
       end
-      
+
       uid do
         @uid ||= begin
           access_token["openid"]
         end
       end
 
-      info do 
-        { 
+      info do
+        {
           :nickname => raw_info['nickname'],
           :name => raw_info['nickname'],
           :image => raw_info['headimgurl'],
         }
       end
-      
+
       extra do
         {
           :raw_info => raw_info
